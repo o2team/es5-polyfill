5eb0f762b3b61d87cf49145bd2137321a7482e03
luckyadam <weitaozsh@gmail.com>
2018-01-15 21:49:30 +0800

    0.0.5

--- dist/polyfill.js
+++ dist/polyfill.js
@@ -819,7 +819,26 @@ if (!Date.prototype.toISOString) {
       };
     }
   })();
+
+  // Node.textContent
+  if (Object.defineProperty
+    && Object.getOwnPropertyDescriptor
+    && Object.getOwnPropertyDescriptor(Element.prototype, 'textContent')
+    && !Object.getOwnPropertyDescriptor(Element.prototype, 'textContent').get) {
+    (function() {
+      var innerText = Object.getOwnPropertyDescriptor(Element.prototype, 'innerText');
+      Object.defineProperty(Element.prototype, 'textContent', {
+        get: function() {
+          return innerText.get.call(this);
+        },
+        set: function(s) {
+          return innerText.set.call(this, s);
+        }
+      });
+    })();
+  }
 })(window);
+
 /**
  * polyfill for DOM Event
  */
